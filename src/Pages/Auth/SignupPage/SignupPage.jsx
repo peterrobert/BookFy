320dab2ab816fb62585fc3bd5250c7aa9a55c032
peterrobert <pwambui93@gmail.com>
2025-10-23 15:44:33 +0300

    Finish the client form cleation

--- src/Pages/Auth/SignupPage/SignupPage.jsx
+++ src/Pages/Auth/SignupPage/SignupPage.jsx
@@ -8,9 +8,7 @@ const SignupPage = () => {
         className="bg-gradient-to-br from-purple-50 to-white py-12 px-5"
       >
         <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold text-black mb-4">
-            Join Appointment Palette
-          </h1>
+          <h1 className="text-4xl font-bold text-black mb-4">Join Bookfy</h1>
           <p className="text-xl text-gray-600 max-w-2xl mx-auto font-light">
             Create your account to start booking appointments or join as a
             professional to grow your business
---
+++ src/Pages/Auth/SignupPage/SignupProffesionalForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SignupProffesionalForm = () => {
+  return <div>This is the professional signup form.</div>;
+};
+
+export default SignupProffesionalForm;
