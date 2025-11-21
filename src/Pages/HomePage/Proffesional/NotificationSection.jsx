26f5e70563fc81088a4125d3137d14f1b59ff195
peterrobert <pwambui93@gmail.com>
2025-11-21 08:01:19 +0300

    `Updated package-lock.json and added missing import in MainContent.jsx`

--- src/Pages/HomePage/Proffesional/NotificationSection.jsx
+++ src/Pages/HomePage/Proffesional/NotificationSection.jsx
@@ -63,7 +63,7 @@ const NotificationSection = () => {
   return (
     <section
       id="notifications-updates"
-      className="grid grid-cols-1 lg:grid-cols-2 gap-6"
+      className="grid grid-cols-1 lg:grid-cols-2 gap-6 mb-6"
     >
       <div
         id="recent-notifications"
