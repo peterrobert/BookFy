21bb09f2a469dad941be7f59be5433d9766857e6
peterrobert <pwambui93@gmail.com>
2025-11-21 10:05:44 +0300

    `Update import paths from components to Components`

--- src/Pages/HomePage/Client/WelcomeSection.jsx
+++ src/Pages/HomePage/Client/WelcomeSection.jsx
@@ -1,5 +1,5 @@
 import { useNavigate } from "react-router-dom";
-import AppButton from "../../../components/AppButton";
+import AppButton from "../../../Components/AppButton";
 const statsData = [
   {
     title: "Upcoming Appointments",
